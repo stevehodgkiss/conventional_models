6332fb56831c8ba63ea91bebe9e97cea7e7b1d32
Steve Hodgkiss <steve@hodgkiss.me.uk>
2011-03-02 14:07:26 +0000

    Bump version to 0.3.0

--- lib/conventional_models/version.rb
+++ lib/conventional_models/version.rb
@@ -1,8 +1,8 @@
 module ConventionalModels #:nodoc:
   module VERSION #:nodoc:
     MAJOR = 0
-    MINOR = 2
-    BUILD = 2
+    MINOR = 3
+    BUILD = 0
  
     STRING = [MAJOR, MINOR, BUILD].join('.')
   end
